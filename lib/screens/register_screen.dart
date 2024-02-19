c63dd71facccc4a31824b26b744e9f4a4144b3ef
SrBrago <ramaspablo1@gmail.com>
2024-02-19 17:11:49 -0600

    Creamos los screens

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_pablo_2/controllers/register_controller.dart';
+import 'package:get/get.dart';
+
+class RegisterScreen extends StatelessWidget {
+  final RegisterController _registerController = Get.put(RegisterController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registro de Usuario'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Nombre'),
+              onChanged: (value) {
+                _registerController.firstName = value;
+              },
+            ),
+            const SizedBox(height: 10.0),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Apellido'),
+              onChanged: (value) {
+                _registerController.lastName = value;
+              },
+            ),
+            const SizedBox(height: 10.0),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Correo Electrónico'),
+              onChanged: (value) {
+                _registerController.email = value;
+              },
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                // Llamar a la función para registrar usuario
+                _registerController.registerUser();
+              },
+              child: const Text('Registrar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/user_list_screen.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: avoid_print, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+
+class UserListScreen extends StatelessWidget {
+  final UserController userController = Get.put(UserController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Usuarios'),
+      ),
+      body: Obx(() => ListView.builder(
+        itemCount: userController.users.length,
+        itemBuilder: (context, index) {
+          var user = userController.users[index];
+          return ListTile(
+            title: Text('${user['firstName']} ${user['lastName']}'),
+            subtitle: Text(user['email']),
+            onTap: () async {
+              var updatedUserData = await Get.to(() => EditProfileScreen(userData: user));
+              if (updatedUserData != null) {
+                userController.updateUser(updatedUserData);
+              }
+            },
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                Get.defaultDialog(
+                  title: 'Confirmar eliminación',
+                  middleText: '¿Estás seguro de que quieres eliminar este usuario?',
+                  textConfirm: 'Eliminar',
+                  textCancel: 'Cancelar',
+                  confirmTextColor: Colors.white,
+                  onConfirm: () {
+                    userController.deleteUser(user['id'].toString());
+                    Get.back();
+                  },
+                  onCancel: () {},
+                );
+              },
+            ),
+          );
+        },
+      )),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 150),
+        child: FloatingActionButton(
+          onPressed: () async {
+            final newUser = await Get.to(() => RegisterScreen());
+            if (newUser != null) {
+              userController.users.add(newUser);
+            }
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
+
