8e5e4a5068261c2fec04d0be90109e9bc1a5c98a
SrBrago <ramaspablo1@gmail.com>
2024-02-19 17:42:56 -0600

    No se

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_pablo_2/screens/user_list_screen.dart';
+import 'package:flutterpablo2/screens/user_list_screen.dart';
 import 'package:get/get.dart';
 
 void main() {
