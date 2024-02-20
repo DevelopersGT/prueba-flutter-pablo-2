788835cc0fdfc3813e15e591fe0b58989ce4d5ee
SrBrago <ramaspablo1@gmail.com>
2024-02-19 18:03:08 -0600

    Parametros de correo

--- lib/controller/register_controller.dart
+++ lib/controller/register_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:uuid/uuid.dart';
 
@@ -12,6 +13,19 @@ class RegisterController extends GetxController {
   }
 
   Future<void> registerUser() async {
+    // Validar si el correo electrónico contiene "@gmail.com"
+    if (!email.contains("@gmail.com")) {
+      // Mostrar un mensaje de error si el correo no contiene "@gmail.com"
+      Get.snackbar(
+        'Error',
+        'El correo electrónico debe ser de tipo @gmail.com',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+      return; // Salir del método si el correo no es válido
+    }
+
     String userId = generateUniqueId();
 
     Map<String, dynamic> newUser = {
